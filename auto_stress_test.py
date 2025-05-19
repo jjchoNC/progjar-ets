c6ee7d15021113072fc0469761a989a48713c069
jjchoNC <5025221001@student.its.ac.id>
2025-05-19 07:59:56 +0700

    feat: add automated stress test script for HTTP operations with varying parameters

---
+++ auto_stress_test.py
@@ -0,0 +1,11 @@
+import subprocess
+
+comb1 = ['post', 'get'] # operation
+comb2 = [10, 50, 100] # volume mb
+comb3 = [1, 5, 50] #client worker pool
+
+for operation in comb1:
+    for size_mb in comb2:
+        for client in comb3:
+            print(f"\nTesting {operation} with {size_mb}MB and {client} clients\n")
+            subprocess.run(["python", "file_client_stress.py", "--operation", operation, "--size", str(size_mb), "--clients", str(client)])
